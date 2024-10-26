cdf77aec3fb377d0d18f45f876d0a6b26189864d
Ankit Arora <ankit@Ankits-MacBook-Pro-2.local>
2024-10-26 07:31:37 +0530

    Added some new features in feat-1 branch

--- A.java
+++ A.java
@@ -1,5 +1,6 @@
 class A {
     public void doSomething() {
         System.out.println("abc");
+        System.out.println("Added more changes for feat-1");
     }
 }
--- B.java
+++ B.java
@@ -1,4 +1,5 @@
 class B {
     System.out.println("commiting from main")
     System.out.println("Another commit")
+    System.out.println("Adding some new features in feat-1 branch");
 }
